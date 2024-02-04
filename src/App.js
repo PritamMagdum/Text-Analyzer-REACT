f1d628a85c5c2d16550415340cbe9dea5c51fed8
Pritam Magdum <pritammagdum24@gmail.com>
2024-02-04 14:32:26 +0530

    Added DarkMode Toggle In Navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 // import About from './components/About';
+import { useState } from 'react';
 
 function App() {
+  const [mode, setMode] = useState("light") // whether dark mode is enable or not
+
+  const toggleMode = () => {
+    if (mode === 'dark') {
+      setMode('light')
+      document.body.style.backgroundColor = "white";
+    } else {
+      setMode('dark')
+      document.body.style.backgroundColor = "gray";
+    }
+  }
+
   return (
     <>
-      <Navbar title="MAGDUM" about="AboutUs"></Navbar>   {/*Navbar with props*/}
+      <Navbar title="MAGDUM" about="AboutUs" mode={mode} toggleMode={toggleMode}></Navbar>   {/*Navbar with props*/}
       {/* <Navbar></Navbar> */}
       <div className="container my-3">
         <TextForm heading="Enter your text to analyse"></TextForm>
